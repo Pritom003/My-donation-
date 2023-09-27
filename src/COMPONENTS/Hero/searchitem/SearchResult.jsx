b28bf6d2f08a1ff7cfc53334dd8d5f08d469ccc4
Your NamePritom003 <njahanpritom65@gmail.com>
2023-09-27 14:39:12 +0600

    modified details

--- src/COMPONENTS/Hero/searchitem/SearchResult.jsx
+++ src/COMPONENTS/Hero/searchitem/SearchResult.jsx
@@ -10,20 +10,16 @@ import { Link } from "react-router-dom";
        
            <div className="bg-white rounded-lg shadow-lg p-6">
                
-               <h2 className="text-2xl font-semibold text-gray-800 mb-4">Program Details</h2>
+               <h2 className="text-2xl font-semibold text-gray-800 mb-4">Campaign Details </h2>
                
-               <p className="text-gray-600 mb-4">
-                   Contribute  for
-                    All initiative, aiming to provide 
-                    quality education,healty food, assential 
-                    treatment  
-                     to underprivileged
-                     children and helped them to live a better life.
-               </p>
+               <ul className="list-disc list-inside text-gray-700 mb-4">
+                <li>Ensure access to quality education for children in need.</li>
+                <li>Provide nutritious meals to combat hunger and malnutrition.</li>
+                <li>Offer essential medical treatment to improve their health and well-being.</li>
+                <li>Empower these children to lead better lives and pursue their dreams.</li>
+            </ul>
              
-               <p className="text-gray-700 mb-4">
-                   Thank you for providing you help and make this world a better one . 
-               </p>
+             
                
                <Link to={'/'}><button   className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-full inline-block transition duration-300 ease-in-out">
                 Go Back
